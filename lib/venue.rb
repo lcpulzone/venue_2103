ee4516fc642c303277af8d83c46b552d95f9802e
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-24 14:00:49 -0700

    Build methods based on specs

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,32 @@
 class Venue
+  attr_reader :name,
+                   :capacity,
+                   :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map do |patron|
+      patron.upcase
+    end
+  end
+
+  def over_capacity?
+    return false if @patrons.count <= 4
+      true
+  end
+
+  def kick_out
+    while over_capacity?
+      @patrons.shift
+    end
+  end
 end
